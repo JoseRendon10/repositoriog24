e1f0f71865852847cd7bccc5fe8671facff3d14a
Jose Rendon <jose.rendon.mt@usa.edu.co>
2022-09-14 18:40:33 -0500

    agregando clase Ejercicio 4

---
+++ src/main/java/com/cliclo3/projectg24/Ejercicio4.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cliclo3.projectg24;
+
+/**
+ *
+ * @author JOSE RENDON
+ */
+public class Ejercicio4 {
+    public void hig(){
+        System.out.println("hola sigo programando");
+    }
+}
