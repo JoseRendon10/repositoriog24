631a45c8ef145b869d2e576b3b04f4b939190a51
Jose Rendon <jose.rendon.mt@usa.edu.co>
2022-09-14 18:59:58 -0500

    agregando funcionlidad al backend

--- src/main/java/com/cliclo3/projectg24/clase5.java
+++ src/main/java/com/cliclo3/projectg24/clase5.java
@@ -8,9 +8,6 @@ package com.cliclo3.projectg24;
  *
  * @author JOSE RENDON
  */
-public class ejercicio1 {
-    public void Metodo(){
-        System.out.println("Logica metodo dos");
-        
-    }
+public class clase5 {
+    
 }
