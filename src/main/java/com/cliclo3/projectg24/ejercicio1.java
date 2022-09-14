e0e7e4c542f8de033aa9f491eac127b5a9b8501f
Jose Rendon <jose.rendon.mt@usa.edu.co>
2022-09-13 19:04:08 -0500

    primer commit del proyecto

---
+++ src/main/java/com/cliclo3/projectg24/ejercicio1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cliclo3.projectg24;
+
+/**
+ *
+ * @author JOSE RENDON
+ */
+public class ejercicio1 {
+    public void Metodo(){
+        System.out.println("Logica metodo");
+        
+    }
+}
